6b1f3a83269930dd5fd905c2cfa318a0fd9703d1
Radu Jipa <radu.t.jipa@gmail.com>
2014-08-11 17:46:09 +0100

    Downloading OS info replaces static URLs on OSX https://github.com/KanoComputing/kano-burners/issues/10

--- src/common/download.py
+++ src/common/download.py
@@ -7,9 +7,13 @@
 # [File description]
 
 
-from time import sleep
+import time
+import json
+import urllib2
+import traceback
+
 from src.common.pySmartDL.pySmartDL import SmartDL, HashFailedException
-from src.common.utils import debugger, read_file_contents_as_lines
+from src.common.utils import debugger
 from src.common.errors import DOWNLOAD_ERROR, MD5_ERROR
 
 
@@ -21,6 +25,8 @@ TEST2_URL = 'http://dev.kano.me/public/squeak.tar.gz'
 #ZIP_MD5_URL = 'http://dev.kano.me/public/Kanux-Beta-v1.1.0.img.gz.md5'
 GZIP_OS_URL = 'http://dev.kano.me/public/Kanux-Beta-v1.1.0.img.gz'
 GZIP_MD5_URL = 'http://dev.kano.me/public/Kanux-Beta-v1.1.0.img.gz.md5'
+
+LATEST_OS_INFO_URL = 'http://downloads.kano.me/public/latest.json'
 
 
 class Downloader(SmartDL):
@@ -40,17 +46,28 @@ class Downloader(SmartDL):
 
 
 def download_kano_os(path, report_progress_ui):
-    # simply make sure the file was not corrupted - not for cryptographic security
-    downloaded_md5 = download_md5(GZIP_MD5_URL, path)
 
-    #downloader = SmartDL(TEST2_URL, dest=path, progress_bar=False)
-    downloader = Downloader(GZIP_OS_URL, dest=path, progress_bar=False)
-    downloader.add_hash_verification('md5', downloaded_md5)
+    # get information about the latest OS version e.g. url, filename, md5 checksum
+    os_info = get_latest_os_info()
+    if not os_info:
+        return None, DOWNLOAD_ERROR
+
+    # give the server some time to breath between requests
+    time.sleep(1)
 
     # the documentation is misleading - if non blocking mode is used,
-    # exceptions can still be thrown
+    # pySmartDL may still throw exceptions
     try:
+        #downloader = SmartDL(TEST2_URL, dest=path, progress_bar=False)
+        downloader = Downloader(os_info['url'], dest=path, progress_bar=False)
+        # simply make sure the file was not corrupted - not for cryptographic security
+        downloader.add_hash_verification('md5', os_info['compressed_md5'])
         downloader.start(blocking=False)
+
+    except KeyError:
+        debugger('[ERROR] the server returned an unsupported json key')
+        traceback.print_exc()
+        return None, DOWNLOAD_ERROR
     except:
         pass
 
@@ -61,46 +78,47 @@ def download_kano_os(path, report_progress_ui):
                            .format(downloader.get_speed(human=True),
                                    downloader.get_eta(human=True),
                                    int(downloader.get_progress() * 100)))
-        sleep(0.3)
+        time.sleep(0.3)
 
     # check if the download finished successfully
     if downloader.isSuccessful():
         debugger('Downloading successfully finished and md5 check passed')
         report_progress_ui(100, 'download completed')
-        return True, None
+        return os_info, None
+
     else:
         # look through the errors that occured and report an MD5 error
         for error in downloader.get_errors():
             if isinstance(error, HashFailedException):
                 debugger('[ERROR] md5 verification failed')
-                return False, MD5_ERROR
+                return None, MD5_ERROR
 
         # for any other errors, report a general message
         debugger('[ERROR] downloading Kano image failed')
-        return False, DOWNLOAD_ERROR
+        return None, DOWNLOAD_ERROR
 
 
-def download_md5(url, path):
-    downloader = Downloader(url, dest=path, progress_bar=False)
+def get_latest_os_info():
+    debugger("Downloading latest OS information")
 
-    # again, making sure we catch any exceptions pySmartDL may throw
+    # we put everything in a try block as urlopen raises URLError
     try:
-        downloader.start(blocking=True)
+        # get latest.json from download.kano.me
+        response = urllib2.urlopen(LATEST_OS_INFO_URL)
+        latest_json = json.load(response)
+
+        # give the server some time to breath between requests
+        time.sleep(1)
+
+        # use the url for the latest os version to get info about the image
+        response = urllib2.urlopen(latest_json['url'] + '.json')
+        os_json = json.load(response)
+
     except:
-        debugger('[ERROR] could not start md5 download')
-        return False, DOWNLOAD_ERROR
+        debugger('[ERROR] downloading OS info failed!')
+        return None
 
-    # check if the MD5 download finished successfully
-    if downloader.isSuccessful():
-        debugger('md5 download was successful')
-
-        # add the md5 file name to the temp dir's path and read it
-        path += url.rsplit('/', 1)[1]
-        lines = read_file_contents_as_lines(path)
-
-        # the md5 checksum is the first 'word' in the first (and only) line
-        downloaded_md5 = lines[0].split()[0]
-        return downloaded_md5
-    else:
-        debugger('[ERROR] md5 download was not successful')
-        return ''
+    # merge the two jsons and return a single info dict result
+    os_info = {key: value for (key, value) in (latest_json.items() + os_json.items())}
+    debugger('os_info is {}'.format(os_info))
+    return os_info
