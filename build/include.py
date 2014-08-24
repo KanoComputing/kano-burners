278c2b1ef84da99786ea01dfa3c0c1b7152b43c9
Radu Jipa <radu.t.jipa@gmail.com>
2014-08-24 21:31:22 +0100

    Added resources include script for building https://github.com/KanoComputing/kano-burners/issues/4

---
+++ build/include.py
@@ -0,0 +1,27 @@
+
+# include.py
+#
+# Copyright (C) 2014 Kano Computing Ltd.
+# License: http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+#
+# [File description]
+
+
+import os
+import glob
+
+def extra_datas(path):
+    def recursive_glob(path, files):
+        for file_path in glob.glob(path):
+            if os.path.isfile(file_path):
+                files.append(os.path.join(os.getcwd(), file_path))
+            recursive_glob('{}/*'.format(file_path), files)
+    files = []
+    recursive_glob('{}/*'.format(path), files)
+    extra_datas = []
+    for f in files:
+        extra_datas.append((f.split('kano-burners')[1][1:], f, 'DATA'))
+    return extra_datas
+
+a.datas += extra_datas(os.path.join(os.getcwd(), '..', '..', 'res'))
+a.datas += extra_datas(os.path.join(os.getcwd(), '..', '..', 'win'))
