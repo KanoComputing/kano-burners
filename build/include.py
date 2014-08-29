ebc83a478c6185eaf1da1d3227f550528e1a0be8
Radu Jipa <radu.t.jipa@gmail.com>
2014-08-29 11:54:36 +0100

    Updated build files https://github.com/KanoComputing/kano-burners/pull/17

--- build/include.py
+++ build/include.py
@@ -16,12 +16,19 @@ def extra_datas(path):
             if os.path.isfile(file_path):
                 files.append(os.path.join(os.getcwd(), file_path))
             recursive_glob('{}/*'.format(file_path), files)
+
     files = []
-    recursive_glob('{}/*'.format(path), files)
     extra_datas = []
+
+    if os.path.isfile(path):
+        files.append(os.path.join(os.getcwd(), path))
+    else:
+        recursive_glob('{}/*'.format(path), files)
+
     for f in files:
         extra_datas.append((f.split('kano-burners')[1][1:], f, 'DATA'))
     return extra_datas
 
 a.datas += extra_datas(os.path.join(os.getcwd(), '..', '..', 'res'))
 a.datas += extra_datas(os.path.join(os.getcwd(), '..', '..', 'win'))
+a.datas += extra_datas(os.path.join(os.getcwd(), '..', '..', 'DISCLAIMER'))
