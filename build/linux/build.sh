be5727db61d049eea68d968129b18f1e70bae271
Radu Jipa <radu.t.jipa@gmail.com>
2015-01-28 16:29:29 +0000

    Refactored the building process into one

---
+++ build/linux/build.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# build.sh
+#
+# Copyright (C) 2014 Kano Computing Ltd.
+# License: http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+#
+#
+# OSX build script
+#
+# This script calls PyInstaller to build Kano Burner
+# from the generated .spec file obtained by running
+# make-spec.sh
+#
+# NOTE: Before running this, make sure the spec file has
+#       been correctly generated and customised as needed.
+#
+# WARNING: Do not run this script with sudo!
+#
+# For more info:
+# http://pythonhosted.org/PyInstaller/#using-spec-files
+
+
+pyinstaller \
+    --distpath=~/Desktop/Kano\ Burner/ \
+    --specpath=~/Desktop/Kano\ Burner/build/ \
+    --workpath=~/Desktop/Kano\ Burner/build/ \
+    --clean \
+    --noconfirm \
+    Kano\ Burner.spec
